6b24d8c3c5a9e094a29ce42359a0af7bac2ab6ad
unknown <cristopherhurtado>
2023-01-23 00:30:04 -0300

    curso api

---
+++ src/routes/employees.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { createEmployee, deleteEmployee, getEmployees, updateEmployee, getEmployee } from "../controllers/employees.controller.js";
+
+const router = Router()
+
+router.get('/employees', getEmployees)
+
+router.get('/employees/:id', getEmployee)
+
+router.post('/employees', createEmployee)
+
+router.put('/employees', updateEmployee)
+
+router.delete('/employees/:id', deleteEmployee)
+
+export default router
